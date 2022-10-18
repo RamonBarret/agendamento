2305bc3ac5481edec631d55e87cb19cffc4207de
Ramon Barreto <ramon_barreto_medrado@hotmail.com>
2022-10-18 10:09:04 -0300

    initial

---
+++ src/pages/SignIn.stories.tsx
@@ -0,0 +1,12 @@
+import { Meta, StoryDb } from '@storybook/react';
+
+import { SignIn } from './SignIn';
+
+export default {
+    title: 'Pages/SignIn'
+    component: Button,
+    args: {},
+    argTypes: {}
+} as Meta
+
+export const Default: StoryObj = {}
